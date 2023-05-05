08f217381f7f291a4e8bff1c285288512a6fd153
DiamondAaronXG <86975161+DiamondAaronXG@users.noreply.github.com>
2023-05-05 09:28:14 -0700

    Update spy.js

--- public/spy.js
+++ public/spy.js
@@ -1,37 +1,28 @@
-  let key;
+let key;
 
-  async function encryptMessage() {
-    const message = document.getElementById("message").value;
-    key = await window.crypto.subtle.generateKey(
-      { name: "AES-GCM", length: 256 },
-      true,
-      ["encrypt", "decrypt"]
-    );
-    const encodedMessage = new TextEncoder().encode(message);
-    const encryptedMessage = await window.crypto.subtle.encrypt(
-      { name: "AES-GCM", iv: window.crypto.getRandomValues(new Uint8Array(12)) },
-      key,
-      encodedMessage
-    );
-    const encryptedArray = new Uint8Array(encryptedMessage);
-    const encryptedHex = Array.from(encryptedArray).map(b => ("00" + b.toString(16)).slice(-2)).join("");
-    document.getElementById("encrypted").value = encryptedHex;
-  }
+function encryptMessage() {
+  const message = document.getElementById("message").value;
+  key = CryptoJS.lib.WordArray.random(32);
+  const iv = CryptoJS.lib.WordArray.random(12);
+  const encrypted = CryptoJS.AES.encrypt(message, key, { iv: iv });
+  const encryptedHex = iv.concat(encrypted.ciphertext).toString(CryptoJS.enc.Hex);
+  document.getElementById("encrypted").value = encryptedHex;
+}
 
-  async function decryptMessage() {
-    const encryptedHex = document.getElementById("encrypted").value;
-    if (!key) {
-      alert("Key is missing. Please encrypt a message first.");
-      return;
-    }
-    const encryptedArray = new Uint8Array(
-      encryptedHex.match(/.{1,2}/g).map(byte => parseInt(byte, 16))
-    );
-    const decryptedArray = await window.crypto.subtle.decrypt(
-      { name: "AES-GCM", iv: encryptedArray.slice(0, 12) },
-      key,
-      encryptedArray
-    );
-    const decryptedMessage = new TextDecoder().decode(decryptedArray);
-    document.getElementById("decrypted").value = decryptedMessage;
+function decryptMessage() {
+  const encryptedHex = document.getElementById("encrypted").value;
+  if (!key) {
+    alert("Key is missing. Please encrypt a message first.");
+    return;
   }
+  const encryptedBytes = CryptoJS.enc.Hex.parse(encryptedHex);
+  const iv = encryptedBytes.slice(0, 16);
+  const ciphertext = encryptedBytes.slice(16);
+  const decrypted = CryptoJS.AES.decrypt(
+    { ciphertext: ciphertext },
+    key,
+    { iv: iv }
+  );
+  const decryptedMessage = decrypted.toString(CryptoJS.enc.Utf8);
+  document.getElementById("decrypted").value = decryptedMessage;
+}
